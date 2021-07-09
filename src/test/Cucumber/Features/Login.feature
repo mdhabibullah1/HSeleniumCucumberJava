840bdf22cc244f8b8bed1d0f697495f9fdac97da
habib <habib@in-school.net>
2021-07-09 20:31:07 +0600

    Support Group Add TC ongoing

--- src/test/Cucumber/Features/Login.feature
+++ src/test/Cucumber/Features/Login.feature
@@ -2,3 +2,14 @@ Feature: Login
   Scenario: Login with valid credentials
     Given I go to this url "http://35.192.231.16/momscradle-admin-webapp/dashboard"
     Given I enter user name and password "bob@jijoty.org" "12345678"
+    When I click on Shopping Service
+    And I click on Support Group
+    And I click on Sessions
+    And I click on Add New
+    And I Select Support Group Name
+    And I Enter Time
+    And I Enter Maximum Size
+    And I Select Location
+    And I select Location Details
+    And I Select Survey
+    And I click on Save Button
--- src/test/java/StepDefination/Login.java
+++ src/test/java/StepDefination/Login.java
@@ -3,11 +3,18 @@ package StepDefination;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.BeforeClass;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 
 public class Login {
 
@@ -21,25 +28,98 @@ public class Login {
         driver = new ChromeDriver();
     }
 
-    @After
-    public void teardown() {
+  /*  @After
+    public void teardown() throws InterruptedException {
+
+
         if (driver != null) {
             driver.quit();
         }
-    }
+    }*/
 // Test comment
     @Given("I go to this url {string}")
     public void i_go_to_this_url(String url) {
+        world.driver.get(url);
+        world.driver.manage().window().maximize();
+    }
 
-    driver.get(url);
+    @When("I click on Shopping Service")
+    public void i_click_on_shopping_service() {
+
+        world.find_WebElement_by_xpath("//span[@class='menu-title'][contains(text(), 'Shopping Services')]").click();
 
     }
+    @When("I click on Support Group")
+    public void i_click_on_support_group() throws InterruptedException {
+        world.find_WebElement_by_xpath("//a[@href='/momscradle-admin-webapp/shopping-services/supportgroup']").click();
+
+        Thread.sleep(5000);
+//        JavascriptExecutor js = (JavascriptExecutor) world;
+//        js.executeScript("window.scrollBy(0,0)", "");
+    }
+    @When("I click on Sessions")
+    public void i_click_on_sessions() {
+      world.find_WebElement_by_xpath("//span[contains(text(), 'Sessions')]").click();
+
+
+    }
+
+    @When("I click on Add New")
+    public void i_click_on_add_new() {
+      world.find_WebElement_by_xpath("//a[@href='/momscradle-admin-webapp/shopping-services/supportgroup/session/new']").click();
+    }
+
+
+    @When("I Enter Time")
+    public void i_enter_time() {
+
+        world.find_WebElement_by_xpath("//input[@name='start time']").sendKeys("06-00am");
+        world.find_WebElement_by_xpath("//input[@name='end time']").sendKeys("06-10am");
+    }
+
+    @When("I Select Support Group Name")
+    public void i_select_support_group_name() {
+        Select SupportGroup =new Select(world.driver.findElement(By.xpath("//h6[contains(text(), 'Support Group')]//parent::div[@class='form-group']//select")));
+        SupportGroup.selectByValue("2");
+    }
+    @When("I Enter Maximum Size")
+    public void i_enter_maximum_size() {
+        world.find_WebElement_by_xpath("//input[@name='start time']").sendKeys("1");
+    }
+
+    @When("I Select Location")
+    public void i_select_location() {
+        Select Location =new Select(world.driver.findElement(By.xpath("//select[@name='location']")));
+        Location.selectByIndex(6);
+    }
+
+    @When("I select Location Details")
+    public void i_select_location_details() {
+        Select LocationDetails =new Select(world.driver.findElement(By.xpath("//select[@name='location details']")));
+        LocationDetails.selectByIndex(1);
+    }
+
+    @When("I Select Survey")
+    public void i_select_survey() {
+        Select LocationDetails =new Select(world.driver.findElement(By.xpath("//select[@class='form-control mt-2 ']")));
+        LocationDetails.selectByIndex(1);
+    }
+
+    @When("I click on Save Button")
+    public void i_click_on_save_button() {
+        world.find_WebElement_by_xpath("//button[contains(text(), 'Save')]").click();
+    }
+
+
     @Given("I enter user name and password {string} {string}")
     public void i_enter_user_name_and_password(String userName, String password) {
-        driver.findElement(By.xpath("//input[@id='email']")).sendKeys(userName);
-        driver.findElement(By.xpath("//input[@name='password']")).sendKeys(password);
-        driver.findElement(By.xpath("//button/span[contains(text(), 'Login')]")).click();
+        world.driver.findElement(By.xpath("//input[@id='email']")).sendKeys(userName);
+        world.driver.findElement(By.xpath("//input[@name='password']")).sendKeys(password);
+        world.driver.findElement(By.xpath("//button/span[contains(text(), 'Login')]")).click();
 
     }
 
 }
+//span[contains(text(), '06:00-06:10')]//parent::td//a[contains(text(), 'Edit')]
+//span[contains(text(), '06:00-06:10')]//parent::td//a[contains(text(), 'Remove')]
+//button[contains(text(), 'Yes')]
